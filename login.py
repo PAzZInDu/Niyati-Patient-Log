f5cb5bd92e342546dd3770e8dea82708141197de
PAzZInDu <pasindu.karunanayaka@pyxeda.ai>
2025-10-15 04:45:47 +0000

    Updated the login.py

--- login.py
+++ login.py
@@ -3,15 +3,14 @@ import authlib
 
 #from app import chat
 
-IMAGE_ADDRESS = "https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/s/h/shutterstock_1512536354.jpg?crop=0%2C317%2C2664%2C1465&wid=1300&hei=715&scl=2.0492307692307694"
-# title
+IMAGE_ADDRESS = "https://www.shutterstock.com/image-photo/doctor-healthcare-medicine-patient-talking-600nw-2191880035.jpg"
 #st.title("Google Login App")
 
 #st.image(IMAGE_ADDRESS)
 #if not st.experimental_user.is_logged_in:
 
 if not st.user.is_logged_in:
-    st.title("Butterfly Classification")
+    st.title("Patient Log")
     st.image(IMAGE_ADDRESS)
     if st.sidebar.button("Log in with Google", type="primary", icon=":material/login:"):
         st.login()
