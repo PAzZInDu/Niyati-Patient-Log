7ff05bbe72989d1c272955b5b7e411e4d942af7e
PAzZInDu <pasindu.karunanayaka@pyxeda.ai>
2025-10-18 07:48:42 +0000

    clear

--- login.py
+++ login.py
@@ -58,7 +58,7 @@ else:
     existing = client.table(st.secrets["SUPABASE_TABLE"]).select("*").eq("patient_id", st.session_state['patient_id']).execute()
 
     if existing.data:
-        st.subheader(f"Welcome Back {st.user.name}")
+        st.subheader(f"Welcome {st.user.name}")
         
         st.info("Please Update info or go to Daily Log")
         update = st.button("Update Info")
@@ -67,17 +67,15 @@ else:
             updated_profile = patient_profile_form(st.session_state["patient_id"])
 
             if updated_profile:
-                client.table(st.secrets["SUPABASE_TABLE"]).insert(updated_profile).execute()
-                #client.table(st.secrets["SUPABASE_TABLE"]).update(updated_profile).eq("patient_id", st.session_state['patient_id']).execute()
-                st.success(f"Profile updated. Please proceed tpo the Daily Log Entry")
-                
+                client.table(st.secrets["SUPABASE_TABLE"]).update(updated_profile).eq("patient_id", st.session_state['patient_id']).execute()
+                st.success(f"Profile updated. Please proceed to the Daily Log Entry")
+                st.rerun()
         
     else:
         # Insert new profile
         new_profile = patient_profile_form(st.session_state["patient_id"])
         try:
             response = client.table(st.secrets["SUPABASE_TABLE"]).insert(new_profile).execute()
-            st.rerun()
 
             # Optional: check if Supabase returned data
             if response.data:
